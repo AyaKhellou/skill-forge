ec930dcefe0a71560a15c0ae8c51166fd068b194
AyaKhellou <aya.khellou.dev@gmail.com>
2026-07-04 17:09:09 +0100

    organizing jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
           <Route path="login" element={<LogIn/>}/>
           <Route path="signup" element={<SignUp/>}/>
         </Route>
-
+        
         <Route element={<Authrequired/>}>
           <Route path="dashboard" element={<Dashboard/>} />
         </Route>
